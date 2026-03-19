872069e350d95ff9640554e65587490a589d08be
cault45 <jake.caulton@gmail.com>
2026-03-19 10:17:38 +0000

    Thu, Mar 19, 2026, 10:17 AM +00:00

---
+++ src/pipeline.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+
+"""
+pipeline.py:
+    main run
+    logs
+    ingesting API data for both
+    write to parquet/csv/json
+clean.py:
+    data validation stuff
+transform.py
+    transformation
+    summary
+"""
+
+def setup_logging():
+
+    
