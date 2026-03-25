bd95d13294760b262805c93ce805739ca1db7456
cault45 <jake.caulton@gmail.com>
2026-03-25 07:55:14 +0000

    Wed, Mar 25, 2026, 7:55 AM +00:00

--- src/pipeline.py
+++ src/pipeline.py
@@ -11,20 +11,6 @@ import transform
 
 logger = logging.getLogger(__name__)
 
-
-"""
-pipeline.py:
-    main run
-    logs
-    ingesting API data for both
-    write to parquet/csv/json
-clean.py:
-    data validation stuff
-transform.py
-    transformation
-    summary
-"""
-
 def setup_logging():
 
     logger.setLevel(logging.INFO)
